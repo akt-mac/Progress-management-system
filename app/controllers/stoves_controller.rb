19f456a91005f2a7718aaa40674ceddf567c0bf4
akt-mac <akitamac+develop@gmail.com>
2019-10-21 06:33:30 +0000

    ストーブstrongparameter追加

--- app/controllers/stoves_controller.rb
+++ app/controllers/stoves_controller.rb
@@ -1,4 +1,6 @@
 class StovesController < ApplicationController
+  before_action :set_user, only: %i(new show create update destroy)
+  before_action :set_stove, only: %i(show create update destroy)
   
   def index
     @stoves = Stove.all
@@ -8,9 +10,21 @@ class StovesController < ApplicationController
     @stove = Stove.new
   end
   
+  def show
+  end
+  
   def create
   end
   
   def update
   end
+  
+  def destroy
+  end
+  
+  private
+  
+    def stove_params
+      params.require(:stove).permit(:note)
+    end
 end
