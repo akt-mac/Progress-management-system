214f52a58f70bbe45a472a9031c687fa9c8674c1
akt-mac <akitamac+develop@gmail.com>
2019-11-14 04:53:53 +0000

    stoves,repairsリソース修正

--- config/routes.rb
+++ config/routes.rb
@@ -8,15 +8,20 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  get '/stoves/:id/edit_progress', to: 'stoves#edit_progress', as: :stove_edit_progress
-  get '/stoves/:id/edit_reminder', to: 'stoves#edit_reminder', as: :stove_edit_reminder
-  get '/repairs/:id/edit_progress', to: 'repairs#edit_progress', as: :repair_edit_progress
-  get '/repairs/:id/edit_reminder', to: 'repairs#edit_reminder', as: :repair_edit_reminder
-  
   get '/stoves/index/:per', to: 'stoves#index_page', as: :stoves_index_per
   get '/repairs/index/:per', to: 'repairs#index_page', as: :repairs_index_per
   
   resources :users
-  resources :stoves
-  resources :repairs
+  resources :stoves do
+    member do
+      get 'edit_progress'
+      get 'edit_reminder'
+    end
+  end
+  resources :repairs do
+    member do
+      get 'edit_progress'
+      get 'edit_reminder'
+    end
+  end
 end
